bdca562fc6589b4251bd1d5ee097227bd9a380c1
Juan Castro <69969436+juan-castro-1@users.noreply.github.com>
2021-03-21 19:23:08 -0300

    Add files via upload

---
+++ 1.ML_fundamentals.py
@@ -0,0 +1,10 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sklearn.linear_model
+import os  
+
+os.chdir('C:/Users/juan_/Dropbox/Mi PC (LAPTOP-H9MAOJRB)/Desktop/Py Projects')
+
+
